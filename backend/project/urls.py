b444f5b3d4c02c7cf4eaccb536306110c96828a3
Valdilene Siqueira <val.siqueira89@gmail.com>
2024-09-07 19:23:32 +0200

    create views for store

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -20,9 +20,13 @@ from django.urls import path
 from upCycleRequest.views import UpCycleRequestListCreateView, UpCycleRequestRetrieveUpdateView
 from item.views import ItemListAPIView
 
+from user.views import ListCreateStoreView, RetrieveUpdateDestroyStoreView
+
 urlpatterns = [
     path('backend/admin/', admin.site.urls),
     path('backend/api/request/', UpCycleRequestListCreateView.as_view(), name='createRequest'),
     path('backend/api/request/<int:id>', UpCycleRequestRetrieveUpdateView.as_view(), name='createRequest'),
     path('backend/api/items/', ItemListAPIView.as_view(), name='items'),
+    path('backend/api/store/', ListCreateStoreView.as_view(), name='store'),
+    path('backend/api/store/<int:id>', RetrieveUpdateDestroyStoreView.as_view(), name='store'),
 ]
